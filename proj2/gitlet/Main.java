9fd77291f4066fd80b6fd0504fc7f1904b30147c
Jeffy Fung <jeffyfungkh@gmail.com>
2021-06-06 15:45:23 +0800

    test commit

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -1,7 +1,11 @@
 package gitlet;
 
+// import gitlet.Commit;
+// import gitlet.Dumpable;
+import gitlet;
+
 /** Driver class for Gitlet, a subset of the Git version-control system.
- *  @author TODO
+ *  @author Jeffrey Fung
  */
 public class Main {
 
@@ -10,6 +14,9 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO: what if args is empty?
+        if (args.length == 0) {
+            Utils.error("Must have at least one argument");
+        }
         String firstArg = args[0];
         switch(firstArg) {
             case "init":
