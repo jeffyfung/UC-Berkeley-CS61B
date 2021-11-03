73ab603be937082683b26314e227b51532523369
Jeffy Fung <jeffyfungkh@gmail.com>
2021-11-04 00:14:55 +0800

    tidy up PersistenceUtils.java

--- proj3/byow/Core/PersistenceUtils.java
+++ proj3/byow/Core/PersistenceUtils.java
@@ -1,21 +1,10 @@
 package byow.Core;
 
-import java.io.BufferedOutputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FilenameFilter;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
-import java.util.Formatter;
 import java.util.List;
 
 
@@ -28,49 +17,14 @@ import java.util.List;
  */
 class PersistenceUtils {
 
-    /** The length of a complete SHA-1 UID as a hexadecimal numeral. */
-    static final int UID_LENGTH = 40;
-
-    /* SHA-1 HASH VALUES. */
-
-    /** Returns the SHA-1 hash of the concatenation of VALS, which may
-     *  be any mixture of byte arrays and Strings. */
-    static String sha1(Object... vals) {
-        try {
-            MessageDigest md = MessageDigest.getInstance("SHA-1");
-            for (Object val : vals) {
-                if (val instanceof byte[]) {
-                    md.update((byte[]) val);
-                } else if (val instanceof String) {
-                    md.update(((String) val).getBytes(StandardCharsets.UTF_8));
-                } else {
-                    throw new IllegalArgumentException("improper type to sha1");
-                }
-            }
-            Formatter result = new Formatter();
-            for (byte b : md.digest()) {
-                result.format("%02x", b);
-            }
-            return result.toString();
-        } catch (NoSuchAlgorithmException excp) {
-            throw new IllegalArgumentException("System does not support SHA-1");
-        }
-    }
-
-    /** Returns the SHA-1 hash of the concatenation of the strings in
-     *  VALS. */
-    static String sha1(List<Object> vals) {
-        return sha1(vals.toArray(new Object[vals.size()]));
-    }
-
     /* FILE DELETION */
 
     /** Deletes FILE if it exists and is not a directory.  Returns true
      *  if FILE was deleted, and false otherwise.  Refuses to delete FILE
      *  and throws IllegalArgumentException unless the directory designated by
-     *  FILE also contains a directory named .gitlet. */
+     *  FILE also contains a directory named .gamesave. */
     static boolean restrictedDelete(File file) {
-        if (!(new File(file.getParentFile(), ".gitlet")).isDirectory()) {
+        if (!file.getParentFile().equals(Engine.GAMESAVE)) {
             throw new IllegalArgumentException("not .gitlet working directory");
         }
         if (!file.isDirectory()) {
@@ -83,33 +37,12 @@ class PersistenceUtils {
     /** Deletes the file named FILE if it exists and is not a directory.
      *  Returns true if FILE was deleted, and false otherwise.  Refuses
      *  to delete FILE and throws IllegalArgumentException unless the
-     *  directory designated by FILE also contains a directory named .gitlet. */
+     *  directory designated by FILE also contains a directory named .gamesave. */
     static boolean restrictedDelete(String file) {
         return restrictedDelete(new File(file));
     }
 
     /* READING AND WRITING FILE CONTENTS */
-
-    /** Return the entire contents of FILE as a byte array.  FILE must
-     *  be a normal file.  Throws IllegalArgumentException
-     *  in case of problems. */
-    static byte[] readContents(File file) {
-        if (!file.isFile()) {
-            throw new IllegalArgumentException("must be a normal file");
-        }
-        try {
-            return Files.readAllBytes(file.toPath());
-        } catch (IOException excp) {
-            throw new IllegalArgumentException(excp.getMessage());
-        }
-    }
-
-    /** Return the entire contents of FILE as a String.  FILE must
-     *  be a normal file.  Throws IllegalArgumentException
-     *  in case of problems. */
-    static String readContentsAsString(File file) {
-        return new String(readContents(file), StandardCharsets.UTF_8);
-    }
 
     /** Write the result of concatenating the bytes in CONTENTS to FILE,
      *  creating or overwriting it as needed.  Each object in CONTENTS may be
@@ -162,12 +95,7 @@ class PersistenceUtils {
 
     /** Filter out all but plain files. */
     private static final FilenameFilter PLAIN_FILES =
-            new FilenameFilter() {
-                @Override
-                public boolean accept(File dir, String name) {
-                    return new File(dir, name).isFile();
-                }
-            };
+            (dir, name) -> new File(dir, name).isFile();
 
     /** Returns a list of the names of all plain files in the directory DIR, in
      *  lexicographic order as Java Strings.  Returns null if DIR does
